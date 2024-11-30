f8af92b615f700031e058ce25865fa0a01148414
emdmed <enrique.darderes@gmail.com>
2024-11-30 23:16:16 +0100

    codemonger demo complete

--- src/components/contextMenu/components/modifyCodeButton.tsx
+++ src/components/contextMenu/components/modifyCodeButton.tsx
@@ -1,24 +1,47 @@
 import { Badge } from "../../ui/badge";
 import { TerminalSquare } from "lucide-react";
 import { elementToObject } from "@/helpers/elementToObject";
+import { useState } from "react";
 
 interface ModifyCodeButtonPops {
   target: HTMLElement;
-  originalTarget: HTMLElement
+  originalTarget: HTMLElement;
 }
 
-const ModifyCodeButton: React.FC<ModifyCodeButtonPops> = ({ target, originalTarget }) => {
+const ModifyCodeButton: React.FC<ModifyCodeButtonPops> = ({
+  target,
+  originalTarget,
+}) => {
+  const [isHover, setIsHover] = useState(false);
+  const [showText, setShowText] = useState(false);
+
   const onClick = () => {
     sendTargetToBackend(originalTarget, target);
   };
 
-  const sendTargetToBackend = async (originalTarget: HTMLElement, target: HTMLElement, ) => {
+  const handleMouseEnter = () => {
+    setIsHover(true);
+    // Delay the appearance of the text
+    setTimeout(() => {
+      setShowText(true);
+    }, 400); // Match the duration of the width transition
+  };
+
+  const handleMouseLeave = () => {
+    setIsHover(false);
+    setShowText(false); // Hide text immediately on hover exit
+  };
+
+  const sendTargetToBackend = async (
+    originalTarget: HTMLElement,
+    target: HTMLElement
+  ) => {
     const parsedOriginalTarget = elementToObject(originalTarget);
-    const parsedNewTarget = elementToObject(target)
+    const parsedNewTarget = elementToObject(target);
 
     const payload = {
       parsedOriginalTarget,
-      parsedNewTarget
+      parsedNewTarget,
     };
 
     const response = await fetch("http://localhost:1216/modify-element", {
@@ -34,10 +57,26 @@ const ModifyCodeButton: React.FC<ModifyCodeButtonPops> = ({ target, originalTarg
 
   return (
     <Badge
-      className="mx-2 border-0 cursor-pointer p-1 bg-cyan-700 text-white"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      id="updatecode"
+      className={`mx-2 border-0 cursor-pointer p-1 bg-cyan-700 text-white hover:bg-cyan-800 transition-all ease-in-out duration-300 h-[25px] ${
+        isHover ? "w-[110px]" : "w-[24px]"
+      }`}
       onClick={onClick}
     >
-      <TerminalSquare size={15} />
+      <div className="flex items-center justify-center overflow-hidden">
+        <TerminalSquare size={15} />
+        {showText && (
+          <span
+            className={`ml-2 transition-opacity duration-500 ${
+              isHover ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            Update code
+          </span>
+        )}
+      </div>
     </Badge>
   );
 };
